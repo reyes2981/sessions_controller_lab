2cd51c9836a28b0038b3fa4881586085f425a8b6
reyes2981 <noe.reyes92@yahoo.com>
2021-03-14 23:41:01 -0500

    commit tree 24fd249284c6e3845d5be9bf03f24419ac79d78a

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+class SessionsController < ApplicationController
+    def new
+    
+    end
+
+    def create
+        session[:username] = params[:username]
+        redirect_to '/'
+    end
+
+end
